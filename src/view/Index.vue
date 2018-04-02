c6366d91e15643aff4e1646699e358e6b16ae485
wkf12058 <598339963@qq.com>
2018-04-02 23:47:49 +0800

    登录

---
+++ src/view/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-row>
+		  <el-button type="primary" @click="goTo('/login')">去登录</el-button>
+		  <el-button type="success">去注册</el-button>
+		</el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '123'
+    }
+  },
+  methods:{
+  	//跳转
+  	goTo(url){
+				this.$router.push({
+					path: url,
+					query:{"id":1,"name":2}
+					});
+  	}
+  	
+  },
+  
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/view/common/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+  	<div>
+  			<h1>登录</h1>
+  	</div>
+  	<div style="width: 80%;margin: 10% 10%; border: solid cornflowerblue 2px;border-radius: 15px;">
+  		<div style="padding: 10%;">
+		 		 <el-input v-model="userName" placeholder="请输入账号"></el-input>
+		 		 <el-input v-model="password" placeholder="请输入密码"></el-input>
+		 		 <br />
+		 		 <br />
+		 		 <el-button type="primary">登录</el-button>
+		 		 <el-button type="success">注册</el-button>
+	 		 </div>
+  	</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    	userName:'',
+      password: '',
+    }
+  },
+    methods:{
+    	
+    },
+}
+</script>
+
+<style scoped>
+</style>
