19a173986930eaa0622f0a69949467ab30df419b
wkf12058 <598339963@qq.com>
2018-05-10 23:15:09 +0800

    个人信息

---
+++ src/view/user/PersonalInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				msg: '个人信息'
+			}
+		},
+		methods: {
+		},
+	}
+</script>
+
+<style scoped>
+
+</style>
